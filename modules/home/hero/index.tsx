d7d3516f2ea814addb1c4f05b948bf1b7c44a152
sup25 <suparnaa51@gmail.com>
2025-01-05 22:43:33 +0545

    first commit

---
+++ modules/home/hero/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="text-red-200">Hero</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,38 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: [
+    "./*.{js,jsx,ts,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      maxWidth: {
+        container: "1280px",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        ".section": {
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          padding: "0 10px",
+        },
+        "@media screen and (min-width: 768px)": {
+          ".section": {
+            padding: "0 30px",
+          },
+        },
+        ".container": {
+          maxWidth: theme("maxWidth.container"),
+        },
+      });
+    }),
+  ],
 } satisfies Config;
