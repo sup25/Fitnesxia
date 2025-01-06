93aff465b381069f968a955df28c7fbb1f2907ac
sup25 <suparnaa51@gmail.com>
2025-01-06 23:41:30 +0545

    feat: banner

--- app/modules/home/banner/index.tsx
+++ app/modules/home/banner/index.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 const Banner = () => {
   return (
-    <div className="section">
+    <div className="section mt-10">
       <div className="container">
-        <p className="text-5xl text-green-200">Heloo hello</p>
+        <div className="flex justify-between gap-5 flex-wrap">
+          <div className="flex flex-col">
+            <div className="text-5xl text-white font-bold ">970k+ More </div>
+            <span className="text-white mt-4">Trusted companies Partner</span>
+          </div>
+
+          <div className="flex gap-7 flex-wrap ">
+            <Image src="/videoask.svg" width={200} height={0} alt="" />
+            <Image src="/hubspot.svg" width={200} height={0} alt="" />
+            <Image src="/mapbox.svg" width={200} height={0} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
