dc7922cb53009acbfcda849b92a97e40286e14d9
sup25 <suparnaa51@gmail.com>
2025-01-07 10:32:16 +0545

    feat:offers

---
+++ app/modules/home/offers/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Offer from "./component/offer";
+
+const Offers = () => {
+  return (
+    <div className="section ">
+      <div className="container">
+        <div className="flex flex-col">
+          <div className="flex w-full justify-between flex-wrap gap-5">
+            <h2 className="heading text-white capitalize leading-normal md:max-w-[515px] max-w-full">
+              The best programs we offers for you
+            </h2>
+            <p className="text-white text-base md:max-w-[430px] max-w-full">
+              we offer a wide range of comprenshive fitness programs designed to
+              carter to individuals of all fitness levels. Our aim to help your
+              achive specific goals & maximize results.
+            </p>
+          </div>
+          <Offer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Offers;
